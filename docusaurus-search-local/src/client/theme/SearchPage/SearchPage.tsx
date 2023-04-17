b8561b1a8e394a67ff52d24412569ac164c51a7e
Joe <joe@statsig.com>
2023-04-17 12:04:12 -0700

    clean

--- docusaurus-search-local/src/client/theme/SearchPage/SearchPage.tsx
+++ docusaurus-search-local/src/client/theme/SearchPage/SearchPage.tsx
@@ -8,11 +8,7 @@ import { usePluralForm } from "@docusaurus/theme-common";
 import { useLocation } from "@docusaurus/router";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import axios from "axios";
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { ChevronDown } from "lucide-react";
+import clsx from "clsx";
 
 import useSearchQuery from "../hooks/useSearchQuery";
 import { fetchIndexes } from "../SearchBar/fetchIndexes";
@@ -316,67 +312,6 @@ function SearchResultItem({
   );
 }
 
-function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-const Accordion = AccordionPrimitive.Root;
-
-const AccordionItem = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
-  // eslint-disable-next-line react/prop-types
->(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item
-    ref={ref}
-    className={cn(
-      "border-b border-b-slate-200 dark:border-b-slate-700",
-      className
-    )}
-    {...props}
-  />
-));
-AccordionItem.displayName = "AccordionItem";
-
-const AccordionTrigger = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
-  // eslint-disable-next-line react/prop-types
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
-    <AccordionPrimitive.Trigger
-      ref={ref}
-      className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <ChevronDown className="h-4 w-4 transition-transform duration-200" />
-    </AccordionPrimitive.Trigger>
-  </AccordionPrimitive.Header>
-));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
-
-const AccordionContent = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
-  // eslint-disable-next-line react/prop-types
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Content
-    ref={ref}
-    className={cn(
-      "data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up overflow-hidden text-sm transition-all",
-      className
-    )}
-    {...props}
-  >
-    <div className="pt-0 pb-4">{children}</div>
-  </AccordionPrimitive.Content>
-));
-AccordionContent.displayName = AccordionPrimitive.Content.displayName;
-
 type Message = {
   type: "apiMessage" | "userMessage";
   message: string;
@@ -408,7 +343,13 @@ function ChatPageContent(): React.ReactElement {
   const [messageState, setMessageState] = useState<{
     messages: Message[];
   }>({
-    messages: [],
+    messages: [
+      {
+        message:
+          "Hi there, I'm Statbot, what would you like to learn about Statsig? Please note that I am an experimental feature and may not be able to answer all of your questions yet. Please verify my output before executing",
+        type: "apiMessage",
+      },
+    ],
   });
 
   const { messages } = messageState;
